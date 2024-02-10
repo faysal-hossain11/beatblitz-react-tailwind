818160a1b41acb257e784e11ff33c794f9d52a58
faysal <devfaysalbd@gmail.com>
2024-02-10 23:38:21 +0600

    complete project

---
+++ src/layouts/FeaturesCard.jsx
@@ -0,0 +1,16 @@
+
+const FeaturesCard = ({icon, title}) => {
+  return (
+    <div className=" flex flex-col items-center text-center p-5 w-full lg:w-1/4 border-2 border-brightColor bg-[#F0F0F6] cursor-pointer rounded-xl gap-2 hover:shadow-[0_3px_10px_rgb(0,0,0,0.5)]">
+      {icon}
+      <h3 className=" font-semibold text-xl"> {title} </h3>
+      <p className=" text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        doloremque aperiam veniam ducimus praesentium necessitatibus harum cum
+        asperiores quos mollitia.
+      </p>
+    </div>
+  );
+};
+
+export default FeaturesCard;
